419cad5a4f2a3b0c3581fb9d70927abf4a803485
GaurangTandon <1gaurangtandon@gmail.com>
2019-05-10 17:40:18 +0530

    fixed faulty hover on navbar add dropdown for participate

--- js/navbar.js
+++ js/navbar.js
@@ -10,18 +10,30 @@ function manageDropdowns() {
 
     dropdowns.forEach(function(dropdown) {
         var toggleBtn = dropdown.querySelector(".dropdown-toggle"),
-            dropdownMenu = dropdown.querySelector(".dropdown-menu");
+            dropdownMenu = dropdown.querySelector(".dropdown-menu"),
+            doNotFadeOutYet = false;
+
+        function fadeOutDropdown() {
+            dropdown.classList.remove(showClass);
+            toggleBtn.setAttribute("expanded", "false");
+            dropdownMenu.classList.remove(showClass);
+            doNotFadeOutYet = false;
+        }
 
         dropdown.addEventListener("mouseenter", function() {
-            this.classList.add(showClass);
+            dropdown.classList.add(showClass);
             toggleBtn.setAttribute("expanded", "true");
             dropdownMenu.classList.add(showClass);
         });
         dropdown.addEventListener("mouseleave", function() {
-            this.classList.remove(showClass);
-            toggleBtn.setAttribute("expanded", "false");
-            dropdownMenu.classList.remove(showClass);
+            setTimeout(function() {
+                if (!doNotFadeOutYet) fadeOutDropdown();
+            }, 100);
         });
+        dropdownMenu.addEventListener("mouseenter", function() {
+            doNotFadeOutYet = true;
+        });
+        dropdownMenu.addEventListener("mouseleave", fadeOutDropdown);
     });
 }
 
