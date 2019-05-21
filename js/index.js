4eb1210de1409c4a657a8ba335b3e6ce4a261d3c
GaurangTandon <1gaurangtandon@gmail.com>
2019-05-21 19:16:15 +0530

    succcesfully setup basic slick carousel

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".testimonial-container").slick({
+        prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
+        nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
+        dots: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
