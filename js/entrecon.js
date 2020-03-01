e94e29d1c700d94f08f96791e10e4a8f94235c8a
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-03-01 09:26:13 +0530

    tried smooth scroll

--- js/entrecon.js
+++ js/entrecon.js
@@ -1,15 +1,15 @@
 $(function() {
-	$('ul.nav a').bind('click',function(event){
-		var $anchor = $(this);
-		/*
-		if you want to use one of the easing effects:*/
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-        }, 1500,'easeInOutExpo');
-        /*
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 1000);*/
-		event.preventDefault();
-	});
+    $("ul.nav a").bind("click", function(event) {
+        let targetPixel = $($(this).attr("href")).offset().left;
+
+        // neither of the following scrolls smooth
+
+        // $(".outer-wrapper").scrollTop(target);
+        $(".outer-wrapper").animate(
+            { scrollLeft: targetPixel },
+            1500,
+            "easeInOutExpo"
+        );
+        event.preventDefault();
+    });
 });
